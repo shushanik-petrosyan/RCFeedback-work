aa7bd4a3349d228b60bb7e2b3789f0b523f4ccf0
admin <admin@DESKTOP-SMPA9FO>
2023-12-05 17:26:43 +0300

    Добавьте файлы проекта.

---
+++ RCFeedback/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+[assembly: ExportFont("Lobster-Regular.ttf", Alias = "Lobster")]
+
+
+namespace RCFeedback
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
