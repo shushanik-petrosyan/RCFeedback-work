2fbdf39c277d72191d2ed5e45a1ee636a626f440
admin <admin@DESKTOP-SMPA9FO>
2023-12-06 23:59:13 +0300

    edit comment writing

--- RCFeedback/MainPage.xaml.cs
+++ RCFeedback/MainPage.xaml.cs
@@ -18,6 +18,25 @@ namespace RCFeedback
             InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false); // скрываем NavigationBar для MainPage
         }
+        void OnEditorTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var editor = (Editor)sender;
+            int lineHeight = 20; // Замените это значение на фактическую высоту строки в вашем редакторе
+
+            // Вычисляем количество строк в текстовом поле
+            int lineCount = editor.Text.Split(new string[] { Environment.NewLine }, StringSplitOptions.None).Length;
+
+            // Задаем минимальную и максимальную высоту
+            int minHeight = 110;
+            int maxHeight = 10000;
+
+            // Вычисляем новую высоту
+            int newHeight = Math.Max(minHeight, Math.Min(maxHeight, lineHeight * lineCount));
+
+            // Устанавливаем новую высоту
+            editor.HeightRequest = newHeight;
+        }
+
 
     }
 }
