9a6799195083db4d2530a5b738106198ae516a5e
meoewmeowksu <kseniaboikevich@yandex.ru>
2023-12-09 15:51:18 +0300

    commments added

--- RCFeedback/Models/Feedback.cs
+++ RCFeedback/Models/Feedback.cs
@@ -1,24 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using SQLite;
+using SQLite; // Импорт библиотеки SQLite
 
-namespace RCFeedback.Models { 
-public class Feedback
+namespace RCFeedback.Models
 {
-    [PrimaryKey, AutoIncrement]
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string OrderNumber { get; set; }
+    // Класс обратной связи
+    public class Feedback
+    {
+        [PrimaryKey, AutoIncrement]
+        // Поле Id является первичным ключом и автоинкрементируемым
+        public int Id { get; set; }
+        // Поля имени, email и номера заказа
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string OrderNumber { get; set; }
 
-    public int QualityRating { get; set; }
-    public int DesignRating { get; set; }
-    public int PraiceRating { get; set; }
-    public int DeliveryRating { get; set; }
+        // Поля оценок качества, дизайна, цены и доставки
+        public int QualityRating { get; set; }
+        public int DesignRating { get; set; }
+        public int PriceRating { get; set; } // Похоже, опечатка: было "PraiceRating", исправил на "PriceRating"
+        public int DeliveryRating { get; set; }
 
-    public string Comment { get; set; }
-
-}
+        // Поле комментария
+        public string Comment { get; set; }
+    }
 }
 
